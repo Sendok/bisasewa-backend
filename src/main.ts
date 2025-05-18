8164e63b68e829d43ad6b17da40993f44ff0a497
Sendok <dhendikdwiprasetyo@gmail.com>
2025-05-18 11:35:35 +0700

    V1 UPDATE

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Set global prefix for all routes
+  app.setGlobalPrefix('api/v1');
+  // Validation pipe
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
